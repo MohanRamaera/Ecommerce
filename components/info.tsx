90595928d7db5eadef697d3a9275d4fc04c7a813
mohansharma916 <mohansharma916@gmail.com>
2024-06-01 11:35:02 +0530

    all set

--- components/info.tsx
+++ components/info.tsx
@@ -2,12 +2,18 @@ import { Product } from "@/types";
 import Currency from "@/components/ui/currency";
 import Button from "./ui/button";
 import { ShoppingCart } from "lucide-react";
-import React from "react";
+import React, { MouseEventHandler } from "react";
+import useCart from "@/hooks/use-cart";
 
 interface InfoProps {
   data: Product;
 }
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+    cart.addItem(data);
+  };
   return (
     <div>
       <h1 className="text-3xl font-bold text-gray-900">{data.name}</h1>
@@ -19,7 +25,7 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       <hr className="my-4" />
 
       <div className="flex items-center mt-10 gap-x-4">
-        <Button className="flex items-center gap-x-2">
+        <Button onClick={onAddToCart} className="flex items-center gap-x-2">
           Add To Cart
           <ShoppingCart />
         </Button>
