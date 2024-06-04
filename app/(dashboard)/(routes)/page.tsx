38ccc6f6199b9d0f16aa6b332c583328f6aad3f1
mohansharma916 <mohansharma916@gmail.com>
2024-06-04 14:33:26 +0530

    billboard added

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -6,16 +6,14 @@ import prismadb from "@/lib/prismadb";
 export const revalidate = 0;
 
 const HomePage = async () => {
-  const products = await prismadb.product.findMany({
-    include: {
-      Image: true,
-    },
-  });
+  const billboard = await getBillboard("df0e09f1-ec53-4f56-96b8-6414c467f7af");
+
   return (
     <>
       <Navbar />
       <Container>
         <div className="pb-10 space-y-10">
+          <Billboard data={billboard} />
           <div className="flex flex-col px-4 gap-y-8 sm:px-6 lg:px-8">
             <ProductList title="Featured Products" items={products} />
           </div>
--- types.ts
+++ types.ts
@@ -2,11 +2,12 @@ import { Decimal } from "@prisma/client/runtime/library";
 
 export interface Product {
   id: string;
-
+  category: Category;
   name: string;
-  price: Decimal;
+  price: string;
   isFeatured: boolean;
-
+  size: Size;
+  color: Color;
   Image: Image[];
 }
 
@@ -16,4 +17,27 @@ export interface Image {
   url: string;
   createdAt: Date;
   updatedAt: Date;
+}
+
+export interface Billboard {
+  id: string;
+  label: string;
+  imageUrl: string;
+}
+
+export interface Category {
+  id: string;
+  name: string;
+  billboard: Billboard;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
 }
